f791c956078f04b60b797c03564afc9e3e176fdd
ckdtjs505 <ckdtjs505@naver.com>
2020-10-06 20:32:06 +0900

    Feat: add scss

--- admin/src/js/index.js
+++ admin/src/js/index.js
@@ -1,3 +1,4 @@
+import "../scss/index.scss";
 import "./test";
 
-alert("123");
+console.log("123");
--- admin/src/webpack.config.js
+++ admin/src/webpack.config.js
@@ -2,10 +2,37 @@ const path = require("path");
 const ENTRY_FILE = path.resolve(__dirname, "js");
 const OUTPUT_DIR = path.join(__dirname, "dist");
 
+console.log(ENTRY_FILE);
+
 module.exports = {
   mode: process.env.WEBPACK_ENV,
   devtool: "inline-source-map",
   entry: ENTRY_FILE,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
     filename: "[name].js"
