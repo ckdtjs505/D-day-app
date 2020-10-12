8186231849ca0a3751d31227be3c8089194b79a2
ckdtjs505 <ckdtjs505@naver.com>
2020-10-12 19:17:02 +0900

    Feat: add start date, pur, state

--- client/src/assets/js/index.js
+++ client/src/assets/js/index.js
@@ -7,14 +7,34 @@ class app {
 
   buildUI() {
     this.date = document.querySelector(".date");
+    this.startDate = document.querySelector(".startDate");
   }
 
   getData() {
     fetch(`http://localhost:3000/app`)
       .then(ele => ele.json())
       .then(data => {
-        console.log(data);
+        const { count, goal, startDate } = data[0];
+        console.log(this.date.querySelector(".pur"));
+        this.date.querySelector(".pur").innerHTML = goal;
+        this.date.querySelector(".state").innerHTML = `${this.numberToString(count + 1)}`;
+        this.startDate.innerHTML =
+          `${startDate.split("-")[0]}.` +
+          `${startDate.split("-")[1]}.` +
+          `${startDate.split("-")[2].split("T")[0]}`;
       });
+  }
+
+  numberToString(num) {
+    if (num >= 1000) {
+      return num;
+    } else if (num >= 100) {
+      return `0${num}`;
+    } else if (num >= 10) {
+      return `00${num}`;
+    } else {
+      return `000${num}`;
+    }
   }
 }
 
