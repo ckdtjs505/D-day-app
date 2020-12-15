2930b1c75d0e0c69ea23639fc406e2c32db8a615
ckdtjs505 <ckdtjs505@naver.com>
2020-12-15 21:07:14 +0900

    feat : 화면 보호기 구현

--- client/src/init.js
+++ client/src/init.js
@@ -1,24 +1,46 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
+  // 화면 보호기 설정
+  if (process.argv.length > 1) {
+    // The /p option tells us to display the screen saver in the tiny preview window in the Screen Saver Settings dialog.
+    if (process.argv[1] === "/p") {
+      app.quit();
+      return;
+    }
+
+    // The /S option is passed when the user chooses Configure from the .scr file context menu (although we don't see this in practice).
+    // The /c:# option is passed when the user clicks Settings... in the Screen Saver Settings dialog.
+    if (process.argv[1] === "/S" || process.argv[1].match(/^\/c/)) {
+      electron.dialog.showMessageBox({
+        message: "This screen saver has no options that you can set.",
+        buttons: ["OK"]
+      });
+      app.quit();
+      return;
+    }
+  }
+
   // Create the browser window.
   const win = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     },
     icon: null,
-    resizable: false
+    kiosk: true,
+    resizable: true,
+    autoHideMenuBar: true, // 메뉴바 숨김
+    fullScreen: true
   });
 
-  // 전체 화면으로 열기
-  win.maximize();
-  // 메뉴 바 제거
-  win.removeMenu();
+  setTimeout(() => {
+    // and load the index.html of the app.
+    win.loadFile("./src/index.html");
+  }, 2000);
 
-  // and load the index.html of the app.
-  win.loadFile("./src/index.html");
   // Open the DevTools.
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
@@ -26,26 +48,25 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
-// // Quit when all windows are closed, except on macOS. There, it's common
-// // for applications and their menu bar to stay active until the user quits
-// // explicitly with Cmd + Q.
-// app.on("window-all-closed", () => {
-//   if (process.platform !== "darwin") {
-//     app.quit();
-//   }
-// });
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-// app.on("activate", () => {
-//   // On macOS it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow();
-//   }
-// });
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
 
-// // In this file you can include the rest of your app's specific main process
-// // code. You can also put them in separate files and require them here.
-
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
 app.setLoginItemSettings({
   openAtLogin: true,
   path: app.getPath("exe")
