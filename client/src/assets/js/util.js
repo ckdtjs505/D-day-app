eba9e76c9639b49ff8fc66cd01b4dcf70fc3bb5e
ckdtjs505 <ckdtjs505@naver.com>
2020-12-20 22:29:23 +0900

    Feat : Util 추가

---
+++ client/src/assets/js/util.js
@@ -0,0 +1,14 @@
+class Util {
+  static addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  // ##.##.## 형식으로 변경
+  static dayToFormat(day) {
+    return (
+      `${day.split("-")[0]}. ` + `${day.split("-")[1]}. ` + `${day.split("-")[2].split("T")[0]}.`
+    );
+  }
+}
