f0d5abc30b90112dc4c9ea73354fb126ddb9e9be
ckdtjs505 <ckdtjs505@naver.com>
2021-01-01 23:35:11 +0900

    Feat: 내용 수정

--- src/models/count.js
+++ src/models/count.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const countSchema = mongoose.Schema({
   startDate: { type: Date, default: Date.now },
   goal: { type: Number, default: 365 },
-  mainText: { type: String, default: "모두 화이팅" }
+  mainText: { type: String, default: "저희 회사는 직원의 안전을 최우선으로 하고 있습니다." }
 });
 
 const Count = mongoose.model("Count", countSchema);
